4e66f48935a52a1e124797ce6c568809cb1f82d8
Eric Cheung <nelsonct2222@gmail.com>
2023-04-28 03:11:25 +0100

    Contact form

---
+++ functions/index.js
@@ -0,0 +1,28 @@
+const functions = require("firebase-functions");
+
+exports.contact = functions.https.onRequest((request, response) => {
+    const { name, email, message } = request.body;
+
+    // Name validation
+    if (!name || name.trim().length === 0) {
+        response.status(400).send("Name must not be blank.");
+        return;
+    }
+
+    // Email validation
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if (!email || !emailRegex.test(email)) {
+        response.status(400).send("Please provide a valid email.");
+        return;
+    }
+
+    // Message validation
+    if (!message || message.trim().length === 0 || message.trim().length > 5000) {
+        response.status(400).send("Message must not be blank and must be a maximum of 5000 characters.");
+        return;
+    }
+
+    // If all validations pass, continue processing the form
+    functions.logger.info("Contacted!", request);
+    response.send("Thank you! I will get in touch with you shortly.");
+});
